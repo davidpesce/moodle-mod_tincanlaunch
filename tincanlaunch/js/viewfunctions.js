davidpesce/moodle-mod_tincanlaunch

commit 6ec949693e91e8fe135cd366f3b761868363b456

Launch link is now constructed using functions for serializing JSON and assembling URLs
Addresses https://github.com/garemoko/moodle-mod_tincanlaunch/issues/2

tincanlaunch/locallib.php
@@ -28,29 +28,46 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-/*
-require_once("$CFG->libdir/filelib.php");
-require_once("$CFG->libdir/resourcelib.php");
-require_once("$CFG->dirroot/mod/tincanlaunch/lib.php");
-*/
 
-/**
- * Does something really useful with the passed things
+
+/*
+ * tincanlaunch_get_launch_url
  *
- * @param array $things
- * @return object
+ * Returns a launch link based on various data from Moodle
+ *
+ * @param none
+ * @return string - the launch link to be used. 
  */
- // 
+
  
 function tincanlaunch_get_launch_url() {
 	global $tincanlaunch, $USER, $CFG;
 	
-	//calculate basic authentication
+	//calculate basic authentication 
 	$basicauth = base64_encode($tincanlaunch->tincanlaunchlrslogin.":".$tincanlaunch->tincanlaunchlrspass);
 	
-	$rtnString = $tincanlaunch->tincanlaunchurl."?endpoint=".$tincanlaunch->tincanlaunchlrsendpoint."&auth=Basic%20$".$basicauth."=&actor={%22name%22:%22".fullname($USER)."%22,%22account%22:{%22homePage%22:%22".$CFG->wwwroot."%22,%22name%22:%22".$USER->id."%22},%22objectType%22:%22Agent%22}";
+	//build the actor object
+	$launchActor = array(
+		"name" => fullname($USER),
+		"account" => array(
+			"homePage" => $CFG->wwwroot,
+			"name" => $USER->id
+		),
+		"objectType" => "Agent"
+	);
 	
-	//QUESTION: should I be using $USER->id, $USER->idnumber or even $USER->username ?
+	//$rtnString = $tincanlaunch->tincanlaunchurl."?endpoint=".$tincanlaunch->tincanlaunchlrsendpoint."&auth=Basic%20".$basicauth."=&actor=".json_encode($launchActor);
+	
+	//build the URL to be returned
+	$rtnString = $tincanlaunch->tincanlaunchurl."?".http_build_query(array(
+	        "endpoint" => $tincanlaunch->tincanlaunchlrsendpoint,
+	        "auth" => "Basic ".$basicauth,
+	        "actor" => json_encode($launchActor)
+	    )
+	);
+	$rtnString = $tincanlaunch->tincanlaunchurl."?endpoint=".."&auth=."=&actor=".;
+	
+	//TODO: QUESTION: should we be using $USER->id, $USER->idnumber or even $USER->username ?
 	
 	return $rtnString;
 }

tincanlaunch/lang/en/tincanlaunch.php
@@ -37,10 +37,7 @@ $string['tincanlaunchname'] = 'Launch link name';
 $string['tincanlaunchname_help'] = 'The name of the launch link as it will appear to the user';
 
 $string['tincanlaunchurl'] = 'Launch URL';
-$string['tincanlaunchurl_help'] = 'The base URL of the Tin Can activity you want to launch';
-
-$string['tincanlaunchurl'] = 'Launch URL';
-$string['tincanlaunchurl_help'] = 'The base URL of the Tin Can activity you want to launch';
+$string['tincanlaunchurl_help'] = 'The base URL of the Tin Can activity you want to launch (including scheme)';
 
 //Start LRS settings
 $string['tincanlaunchlrsfieldset'] = 'LRS settings (to be moved to global settings in the final version)';

commit 48b5436698e34e2e4d5c751159d90d25dfc08481

fixes some bugs introduced in 6ec949693e91e8fe135cd366f3b761868363b456

tincanlaunch/js/viewfunctions.js
@@ -1,0 +1,38 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A collection of JavaScript functions to support view.php
+ *
+ *
+ * @package mod_tincanlaunch
+ * @copyright  2013 Andrew Downes
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+(function(){
+  "use strict";
+  	//create a functional object
+	function mod_tincanlaunch_view (){};
+	mod_tincanlaunch_view.prototype={
+		somefunction : function() {
+			
+		},
+		someotherfunction : function() {
+			
+		}
+	}
+  
+})();

tincanlaunch/locallib.php
@@ -56,16 +56,15 @@ function tincanlaunch_get_launch_url() {
 		"objectType" => "Agent"
 	);
 	
-	//$rtnString = $tincanlaunch->tincanlaunchurl."?endpoint=".$tincanlaunch->tincanlaunchlrsendpoint."&auth=Basic%20".$basicauth."=&actor=".json_encode($launchActor);
-	
 	//build the URL to be returned
 	$rtnString = $tincanlaunch->tincanlaunchurl."?".http_build_query(array(
 	        "endpoint" => $tincanlaunch->tincanlaunchlrsendpoint,
 	        "auth" => "Basic ".$basicauth,
 	        "actor" => json_encode($launchActor)
-	    )
+	    ), 
+	    '', 
+	    '&'
 	);
-	$rtnString = $tincanlaunch->tincanlaunchurl."?endpoint=".."&auth=."=&actor=".;
 	
 	//TODO: QUESTION: should we be using $USER->id, $USER->idnumber or even $USER->username ?
 	

tincanlaunch/view.php
@@ -18,9 +18,6 @@
 /**
  * Prints a particular instance of tincanlaunch
  *
- * You can have a rather longer description of the file as well,
- * if you like, and it can span multiple lines.
- *
  * @package mod_tincanlaunch
  * @copyright  2013 Andrew Downes
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -63,6 +60,7 @@ $PAGE->set_context($context);
 //$PAGE->set_cacheable(false);
 //$PAGE->set_focuscontrol('some-html-id');
 //$PAGE->add_body_class('tincanlaunch-'.$somevar);
+$PAGE->requires->jquery();
 
 // Output starts here
 echo $OUTPUT->header();
@@ -71,9 +69,32 @@ if ($tincanlaunch->intro) { // Conditions to show the intro can change to look f
     echo $OUTPUT->box(format_module_intro('tincanlaunch', $tincanlaunch, $cm->id), 'generalbox mod_introbox', 'tincanlaunchintro');
 }
 
+//Insert JavaScript functions
 
+echo "<script src='js/viewfunctions.js'></script>
+<script>var myViewFunctions = new mod_tincanlaunch_view();</script>";
 
-echo "The activity has opened in a new window. <script>window.open('".tincanlaunch_get_launch_url()."');</script>";
+//TODO: localisation of launch table
+
+//Get a list of registrations from the LRS State
+
+//Create a table of registrations, each with a launch link. 
+//On clicking a launch link, launch the experience with the correct registration
+
+//generate a registration id for any new attempt
+
+$registrationid = 'foo';
+
+//Add a new attempt link below the table
+//On clicking new attempt, save the registration details to the LRS State and launch a new attempt
+
+?>
+
+<a onclick="myViewFunctions.saveNewRegistration('<?php echo $registrationid ?>')">New Attempt</a>;
+<?php
+echo tincanlaunch_get_launch_url();
+echo "The activity has opened in a new window. 
+<script>window.open('".tincanlaunch_get_launch_url()."');</script>";
 
 // Finish the page
 echo $OUTPUT->footer();