66239d634b53abdfacf2647ded72b0a969558935
Andrew Downes <mrdownes@hotmail.com>
2016-06-03 09:35:47 -0500

    update version

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016052300;      // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2016060300;      // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015111000;      // Requires Moodle 3.0 version.
 $plugin->cron      = 0;               // Period for cron to check this module (secs)
 $plugin->component = 'mod_tincanlaunch'; // To check on upgrade, that module sits in correct place
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.3RC1 (Build: 2016052300)';
+$plugin->release = '1.3RC2 (Build: 2016060300)';
