5e70276f361e12a17ae7bc416e991eb249527136
David Pesce <davidpesce@gmail.com>
2020-06-24 17:40:00 -0400

    removed unused vars/functions (#191)
    * removed unused vars/functions

--- lib.php
+++ lib.php
@@ -71,11 +71,10 @@ function tincanlaunch_supports($feature) {
  * will create a new instance and return the id number
  * of the new instance.
  * @param object $tincanlaunch An object from the form in mod_form.php
- * @param mod_tincanlaunch_mod_form $mform
  * @global moodle_database $DB
  * @return int The id of the newly inserted tincanlaunch record
  */
-function tincanlaunch_add_instance(stdClass $tincanlaunch, mod_tincanlaunch_mod_form $mform = null) {
+function tincanlaunch_add_instance(stdClass $tincanlaunch) {
     global $DB;
 
     $tincanlaunch->timecreated = time();
@@ -111,10 +110,9 @@ function tincanlaunch_add_instance(stdClass $tincanlaunch, mod_tincanlaunch_mod_
  * will update an existing instance with new data.
  *
  * @param object $tincanlaunch An object from the form in mod_form.php
- * @param mod_tincanlaunch_mod_form $mform
  * @return boolean Success/Fail
  */
-function tincanlaunch_update_instance(stdClass $tincanlaunch, mod_tincanlaunch_mod_form $mform = null) {
+function tincanlaunch_update_instance(stdClass $tincanlaunch) {
     global $DB;
 
     $tincanlaunch->timemodified = time();
@@ -191,7 +189,8 @@ function tincanlaunch_delete_instance($id) {
     }
 
     // Determine if there is a record of this (ever) in the tincanlaunch_lrs table.
-    $tincanlaunchlrsid = $DB->get_field('tincanlaunch_lrs', 'id', array('tincanlaunchid' => $id), $strictness = IGNORE_MISSING);
+    $strictness = IGNORE_MISSING;
+    $tincanlaunchlrsid = $DB->get_field('tincanlaunch_lrs', 'id', array('tincanlaunchid' => $id), $strictness);
     if ($tincanlaunchlrsid) {
         // If there is, delete it.
         $DB->delete_records('tincanlaunch_lrs', array('id' => $tincanlaunchlrsid));
@@ -211,24 +210,11 @@ function tincanlaunch_delete_instance($id) {
  *
  * @return stdClass|null
  */
-function tincanlaunch_user_outline($course, $user, $mod, $tincanlaunch) {
+function tincanlaunch_user_outline() {
     $return = new stdClass();
     $return->time = 0;
     $return->info = '';
     return $return;
-}
-
-/**
- * Prints a detailed representation of what a user has done with
- * a given particular instance of this module, for user activity reports.
- *
- * @param stdClass $course the current course record
- * @param stdClass $user the record of the user we are generating report for
- * @param cm_info $mod course module info
- * @param stdClass $tincanlaunch the module instance record
- * @return void, is supposed to echp directly
- */
-function tincanlaunch_user_complete($course, $user, $mod, $tincanlaunch) {
 }
 
 /**
@@ -238,34 +224,8 @@ function tincanlaunch_user_complete($course, $user, $mod, $tincanlaunch) {
  *
  * @return boolean
  */
-function tincanlaunch_print_recent_activity($course, $viewfullnames, $timestart) {
+function tincanlaunch_print_recent_activity() {
     return false;  // True if anything was printed, otherwise false.
-}
-
-/**
- * Prepares the recent activity data
- *
- * This callback function is supposed to populate the passed array with
- * custom activity records. These records are then rendered into HTML via
- * {@link tincanlaunch_print_recent_mod_activity()}.
- *
- * @param array $activities sequentially indexed array of objects with the 'cmid' property
- * @param int $index the index in the $activities to use for the next record
- * @param int $timestart append activity since this time
- * @param int $courseid the id of the course we produce the report for
- * @param int $cmid course module id
- * @param int $userid check for a particular user's activity only, defaults to 0 (all users)
- * @param int $groupid check for a particular group's activity only, defaults to 0 (all groups)
- * @return void adds items into $activities and increases $index
- */
-function tincanlaunch_get_recent_mod_activity(&$activities, &$index, $timestart, $courseid, $cmid, $userid = 0, $groupid = 0) {
-}
-
-/**
- * Prints single activity item prepared by {@see tincanlaunch_get_recent_mod_activity()}
- * @return void
- */
-function tincanlaunch_print_recent_mod_activity($activity, $courseid, $detail, $modnames, $viewfullnames) {
 }
 
 /**
@@ -303,7 +263,7 @@ function tincanlaunch_get_extra_capabilities() {
  * @param stdClass $context
  * @return array of [(string)filearea] => (string)description
  */
-function tincanlaunch_get_file_areas($course, $cm, $context) {
+function tincanlaunch_get_file_areas() {
     $areas = array();
     $areas['content'] = get_string('areacontent', 'scorm');
     $areas['package'] = get_string('areapackage', 'scorm');
@@ -318,16 +278,13 @@ function tincanlaunch_get_file_areas($course, $cm, $context) {
  *
  * @param file_browser $browser
  * @param array $areas
- * @param stdClass $course
- * @param stdClass $cm
  * @param stdClass $context
  * @param string $filearea
- * @param int $itemid
  * @param string $filepath
  * @param string $filename
  * @return file_info instance or null if not found
  */
-function tincanlaunch_get_file_info($browser, $areas, $course, $cm, $context, $filearea, $itemid, $filepath, $filename) {
+function tincanlaunch_get_file_info($browser, $areas, $context, $filearea, $filepath, $filename) {
     global $CFG;
 
     if (!has_capability('moodle/course:managefiles', $context)) {
@@ -365,18 +322,16 @@ function tincanlaunch_get_file_info($browser, $areas, $course, $cm, $context, $f
  * @param stdClass $context context object
  * @param string $filearea file area
  * @param array $args extra arguments
- * @param bool $forcedownload whether or not force download
  * @param array $options additional options affecting the file serving
  * @return bool false if file not found, does not return if found - just send the file
  */
-function tincanlaunch_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options = array()) {
+function tincanlaunch_pluginfile($course, $cm, $context, $filearea, $args, array $options = array()) {
 
     if ($context->contextlevel != CONTEXT_MODULE) {
         return false;
     }
 
     require_login($course, true, $cm);
-    $canmanageactivity = has_capability('moodle/course:manageactivities', $context);
 
     $filename = array_pop($args);
     $filepath = implode('/', $args);
@@ -440,35 +395,8 @@ function tincanlaunch_export_contents($cm, $baseurl) {
     return $contents;
 }
 
-// Navigation API.
-
-/**
- * Extends the global navigation tree by adding tincanlaunch nodes if there is a relevant content
- *
- * This can be called by an AJAX request so do not rely on $PAGE as it might not be set up properly.
- *
- * @param navigation_node $navref An object representing the navigation tree node of the tincanlaunch module instance
- * @param stdClass $course
- * @param stdClass $module
- * @param cm_info $cm
- */
-function tincanlaunch_extend_navigation(navigation_node $navref, stdclass $course, stdclass $module, cm_info $cm) {
-}
-
-/**
- * Extends the settings navigation with the tincanlaunch settings
- *
- * This function is called when the context for the page is a tincanlaunch module. This is not called by AJAX
- * so it is safe to rely on the $PAGE.
- *
- * @param settings_navigation $settingsnav {@link settings_navigation}
- * @param navigation_node $tincanlaunchnode {@link navigation_node}
- */
-function tincanlaunch_extend_settings_navigation(settings_navigation $settingsnav, navigation_node $tincanlaunchnode = null) {
-}
-
 // Called by Moodle core.
-function tincanlaunch_get_completion_state($course, $cm, $userid, $type) {
+function tincanlaunch_get_completion_state($cm, $type) {
     global $DB;
     $result = $type; // Default return value.
 
@@ -573,9 +501,6 @@ function tincanlaunch_process_new_package($tincanlaunch) {
     // This is unlikely as it should have been checked when the file was validated.
     if ($manifestfile = $fs->get_file($context->id, 'mod_tincanlaunch', 'content', 0, '/', 'tincan.xml')) {
         $xmltext = $manifestfile->get_content();
-
-        $defaultorgid = 0;
-        $firstinorg = 0;
 
         $pattern = '/&(?!\w{2,6};)/';
         $replacement = '&amp;';
