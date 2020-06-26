c85df3171948f505bc84ada41826fa2c1bf3b85b
David Pesce <davidpesce@gmail.com>
2020-06-26 09:50:48 -0400

    added settings and new lang strings

--- lang/en/tincanlaunch.php
+++ lang/en/tincanlaunch.php
@@ -139,3 +139,7 @@ $string['tincanlaunch:addinstance'] = 'Add a new xAPI activity to a course';
 
 $string['expirecredentials'] = 'Expire credentials';
 $string['checkcompletion'] = 'Check Completion';
+
+// User profile fields
+$string['profilefields'] = 'User profile fields to sync to Agent Profile';
+$string['profilefields_desc'] ='If selected, the Moodle user profile fields will be sent to the LRS under the actors agent profile.';
