157dabb751dc76ec4b53b1c01cf47a7acf9b1325
David Pesce <david.pesce@exputo.com>
2023-04-05 20:40:23 +0000

    convert AMD to ESM

--- amd/src/launch.js
+++ amd/src/launch.js
@@ -18,117 +18,113 @@
  *
  * @package
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author    David Pesce  (david.pesce [at] exputo [dt] com)
+ * @author    David Pesce  <david.pesce@exputo.com>
  * @module    mod_tincanlaunch/launch
  */
 
-define(['jquery', 'core/str'], function($, Str) {
+import $ from 'jquery';
+import * as Str from 'core/str';
 
-    var id = '';
-    var n = '';
+let id = '';
+let n = '';
 
-    var SELECTORS = {
-        ATTEMPT_PROGRESS: '#tincanlaunch_attemptprogress',
-        ATTEMPT_TABLE: '#tincanlaunch_attempttable',
-        COMPLETION_CHECK: '#tincanlaunch_completioncheck',
-        EXIT: '#tincanlaunch_exit',
-        LAUNCH_FORM: '#launchform',
-        MAINCONTENT: '#maincontent',
-        NEW_ATTEMPT: '#tincanlaunch_newattempt',
-        NEW_ATTEMPT_LINK: '[id^=tincanlaunch_newattemptlink-]',
-        REATTEMPT: '[id^=tincanrelaunch_attempt-]',
-        REGISTRATION: '#launchform_registration',
-        STATUSDIV: '#tincanlaunch_status',
-        STATUSPARA: '#tincanlaunch_status_para'
-    };
+let SELECTORS = {
+    ATTEMPT_PROGRESS: '#tincanlaunch_attemptprogress',
+    ATTEMPT_TABLE: '#tincanlaunch_attempttable',
+    COMPLETION_CHECK: '#tincanlaunch_completioncheck',
+    EXIT: '#tincanlaunch_exit',
+    LAUNCH_FORM: '#launchform',
+    MAINCONTENT: '#maincontent',
+    NEW_ATTEMPT: '#tincanlaunch_newattempt',
+    NEW_ATTEMPT_LINK: '[id^=tincanlaunch_newattemptlink-]',
+    REATTEMPT: '[id^=tincanrelaunch_attempt-]',
+    REGISTRATION: '#launchform_registration',
+    STATUSDIV: '#tincanlaunch_status',
+    STATUSPARA: '#tincanlaunch_status_para'
+};
 
-    var Launch = {
-        init: function() {
-            var self = this;
+export const init = () => {
 
-            // Retrieve id and n URL parameters
-            var urlparams = new URLSearchParams(window.location.search);
-            id = urlparams.get('id');
-            n = urlparams.get('n');
+    // Retrieve id and n URL parameters
+    let urlparams = new URLSearchParams(window.location.search);
+    id = urlparams.get('id');
+    n = urlparams.get('n');
 
-            // Iterate over table registrations and add necessary values.
-            $(SELECTORS.REATTEMPT).each(function() {
-                var registrationid = $(this).attr('id').substring(23);
+    // Iterate over table registrations and add necessary values.
+    $(SELECTORS.REATTEMPT).each(function() {
+        var registrationid = $(this).attr('id').substring(23);
 
-                // Listen for keyUp event.
-                $(this).keyup(function(e) {
-                    self.keyTest(e.keyCode, registrationid);
-                });
+        // Listen for keyUp event.
+        $(this).keyup(function(e) {
+            keyTest(e.keyCode, registrationid);
+        });
 
-                // Listen for click event.
-                $(this).click(function() {
-                    self.launchExperience(registrationid);
-                });
+        // Listen for click event.
+        $(this).click(function() {
+            launchExperience(registrationid);
+        });
 
-                // Add tabindex and cursor.
-                $(this).attr('tabindex', '0');
-                $(this).attr('class', 'btn btn-primary');
-            });
+        // Add tabindex and cursor.
+        $(this).attr('tabindex', '0');
+        $(this).attr('class', 'btn btn-primary');
+    });
 
-            // Add details to new attempt link.
-            var newregistrationid = $(SELECTORS.NEW_ATTEMPT_LINK).attr('id').substring(28);
-            $(SELECTORS.NEW_ATTEMPT_LINK).attr('tabindex', '0');
+    // Add details to new attempt link.
+    var newregistrationid = $(SELECTORS.NEW_ATTEMPT_LINK).attr('id').substring(28);
+    $(SELECTORS.NEW_ATTEMPT_LINK).attr('tabindex', '0');
 
-            $(SELECTORS.NEW_ATTEMPT_LINK).click(function() {
-                self.launchExperience(newregistrationid);
-            });
+    $(SELECTORS.NEW_ATTEMPT_LINK).click(function() {
+        launchExperience(newregistrationid);
+    });
 
-            $(SELECTORS.NEW_ATTEMPT_LINK).keyup(function(e) {
-                self.keyTest(e.keyCode, newregistrationid);
-            });
+    $(SELECTORS.NEW_ATTEMPT_LINK).keyup(function(e) {
+        keyTest(e.keyCode, newregistrationid);
+    });
 
-            // Add status para.
-            var statuspara = $("<p></p>").attr("id", "tincanlaunch_status_para");
+    // Add status para.
+    var statuspara = $("<p></p>").attr("id", "tincanlaunch_status_para");
 
-            // Add completion span.
-            var completionspan = $("<span>").attr("id", "tincanlaunch_completioncheck");
-            $(SELECTORS.STATUSDIV).append(statuspara, completionspan);
+    // Add completion span.
+    var completionspan = $("<span>").attr("id", "tincanlaunch_completioncheck");
+    $(SELECTORS.STATUSDIV).append(statuspara, completionspan);
 
-            // Periodically check completion
-            setInterval(function() {
-                $(SELECTORS.COMPLETION_CHECK).load('completion_check.php?id=' + id + '&n=' + n);
-            }, 30000); // TODO: make this interval a configuration setting.
+    // Periodically check completion
+    setInterval(function() {
+        $(SELECTORS.COMPLETION_CHECK).load('completion_check.php?id=' + id + '&n=' + n);
+    }, 30000); // TODO: make this interval a configuration setting.
+};
+
+const keyTest = (keycode, registrationid) => {
+    if (keycode === 13 || keycode === 32) {
+        launchExperience(registrationid);
+    }
+};
+
+const launchExperience = (registrationid) => {
+    let stringsToRetrieve = [
+        {
+            key: 'tincanlaunch_progress',
+            component: 'tincanlaunch'
         },
-        keyTest: function(keycode, registrationid) {
-            var self = this;
-            if (keycode === 13 || keycode === 32) {
-                self.launchExperience(registrationid);
-            }
-        },
-        launchExperience: function(registrationid) {
-            var stringsToRetrieve = [
-                {
-                    key: 'tincanlaunch_progress',
-                    component: 'tincanlaunch'
-                },
-                {
-                    key: 'returntocourse',
-                    component: 'tincanlaunch'
-                }
-            ];
-            $(SELECTORS.REGISTRATION).val(registrationid);
-            $(SELECTORS.LAUNCH_FORM).submit();
-            $(SELECTORS.NEW_ATTEMPT).remove();
-            $(SELECTORS.ATTEMPT_TABLE).remove();
-
-            Str.get_strings(stringsToRetrieve)
-                .done(function(s) {
-                    // Attempt in progress.
-                    $(SELECTORS.STATUSPARA).text(s[0]);
-
-                    // Return to course.
-                    var exitpara = $("<p></p>").attr("id", SELECTORS.EXIT);
-                    exitpara.html("<a href='complete.php?id=" + id + "&n=" + n + "'>" + s[1] + "</a>");
-                    $(SELECTORS.STATUSPARA).after(exitpara);
-            });
+        {
+            key: 'returntocourse',
+            component: 'tincanlaunch'
         }
+    ];
+    $(SELECTORS.REGISTRATION).val(registrationid);
 
-    };
+    $(SELECTORS.LAUNCH_FORM).submit();
+    $(SELECTORS.NEW_ATTEMPT).remove();
+    $(SELECTORS.ATTEMPT_TABLE).remove();
 
-    return Launch;
-});
+    Str.get_strings(stringsToRetrieve)
+        .done(function(s) {
+            // Attempt in progress.
+            $(SELECTORS.STATUSPARA).text(s[0]);
+
+            // Return to course.
+            var exitpara = $("<p></p>").attr("id", SELECTORS.EXIT);
+            exitpara.html("<a href='complete.php?id=" + id + "&n=" + n + "'>" + s[1] + "</a>");
+            $(SELECTORS.STATUSPARA).after(exitpara);
+    });
+};
