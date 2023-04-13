d8afd6c1267cbea819f05b952baea7c927b68f6a
David Pesce <david.pesce@exputo.com>
2023-04-13 17:06:11 +0000

    release version bump

--- version.php
+++ version.php
@@ -28,9 +28,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023040300;      // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2023041000;      // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051700;      // Requires Moodle 3.11 version.
+$plugin->supported = [311, 401];
 $plugin->cron      = 0;               // Period for cron to check this module (secs).
 $plugin->component = 'mod_tincanlaunch'; // To check on upgrade, that module sits in correct place.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.4 (Build: 2023040300)';
+$plugin->release = '1.5';
